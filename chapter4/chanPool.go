11f7d93f4e848139a37474aaf95b706d91707541
囧囧男 <fei110339@163.com>
2019-03-04 14:46:32 +0800

    无缓冲channel也可以实现Pool

--- chapter4/chanPool.go
+++ chapter4/chanPool.go
@@ -12,6 +12,7 @@ func main() {
     )
     chReq := make(chan string, GOROUTINE_COUNT)
     chRes := make(chan int, TASK_COUNT)
+    done := make(chan bool)  //创建一个无缓存的channel，由于该channel是阻塞的，在所有的数据未取出前，主程序就不退出
     // 子协程
     go func() {
         urls := make([]string, TASK_COUNT)
@@ -19,20 +20,22 @@ func main() {
             fmt.Sprintf("http://www.%d.com", i)
             urls[i] = fmt.Sprintf("http://www.%d.com", i)
             chReq <- urls[i]
+            done <- true
             fmt.Println("new URL is %s", chReq)
         }
     }()
     for i := 0; i < GOROUTINE_COUNT; i++ {
         // time.Sleep(1)
+        <-done
         go func() {
             url := <-chReq
             fmt.Println("new URL is %s", url)
             chRes <- 0
         }()
     }
-    // 先执行主进程
-    for i := 0; i < TASK_COUNT; i++ {
-        d := <-chRes
-        fmt.Sprintf("res is %d", d)
-    }
+    // 执行主进程
+    // for i := 0; i < TASK_COUNT; i++ {
+    //     d := <-chRes
+    //     fmt.Sprintf("res is %d", d)
+    // }
 }
